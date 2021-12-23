63998ed98a2fce85de2ae52a95722524177b7368
gupsc <gupsc@LAPTOP-VDDHHNEH>
2021-12-22 22:58:22 -0300

    Correção do pom.xml para ler as libs do diretorio local Correção da classe DTO com referencia a uma classe que não existe mais

--- sysadmin/src/main/java/br/silveira/conciliador/organizational/dto/CompanyCostValuesRequestDto.java
+++ sysadmin/src/main/java/br/silveira/conciliador/organizational/dto/CompanyCostValuesRequestDto.java
@@ -2,14 +2,12 @@ package br.silveira.conciliador.organizational.dto;
 
 import java.util.List;
 
-import br.silveira.conciliador.common.dto.RestResponseCommonDto;
 import br.silveira.conciliador.common.enums.MktPlaceEnum;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode(callSuper=false)
 @Data
-public class CompanyCostValuesRequestDto extends RestResponseCommonDto{
+public class CompanyCostValuesRequestDto{
 
 	private String id;
 	
