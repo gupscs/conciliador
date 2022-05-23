7e1d50cd94a48c8931eb2526ce9b5fcabfb11f98
Gustavo Silveira <gupscs@gmail.com>
2022-05-22 22:32:15 -0300

    rota de liberação

---
+++ api-gateway/src/main/java/br/silveira/conciliador/apigateway/config/RouterValidator.java
@@ -0,0 +1,25 @@
+package br.silveira.conciliador.apigateway.config;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouterValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/oauth/oauth/token",
+            "/organizational/organizational/registerCheck/",
+            "/organizational/organizational/register",
+            "/oauth/check_token",
+            "/_ah/**"
+            
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
